a1b6813082a6aa35d9bd2c5e31ed01e879fdbd20
Diego Alfonso <darellano1@ucol.mx>
2025-03-17 22:37:05 -0600

    primer avance del proyecto

---
+++ scripts/btn.js
@@ -0,0 +1,5 @@
+//Funcion de botones
+document.getElementById('lucBeb').addEventListener('click', ()=>{
+    const section = document.getElementById('drinks');
+    section.scrollIntoView({ behavior: 'smooth' });
+});
