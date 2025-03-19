27473b5510f68df8c11f1fa77a8b0a8e0c6cd5de
RAH-013 <ranguiano7@ucol.mx>
2025-03-18 18:18:22 -0600

    Revisión

--- scripts/btn.js
+++ scripts/btn.js
@@ -1,34 +1,71 @@
 //Funcion de botones
-document.getElementById('lucBeb').addEventListener('click', ()=>{
-    const section = document.getElementById('drinks');
-    section.scrollIntoView({ behavior: 'smooth' });
+document.getElementById("lucBeb").addEventListener("click", () => {
+  const section = document.getElementById("drinks");
+  section.scrollIntoView({ behavior: "smooth" });
 });
 
+document.getElementById("lucPla").addEventListener("click", () => {
+  const section = document.getElementById("food");
+  section.scrollIntoView({ behavior: "smooth" });
+});
 
 //Pop up de los platillos
-document.getElementById('meal').addEventListener('click', () => {
-    const showMeals = document.getElementById('show-meal');
-    showMeals.innerHTML = `
-    <dialog id="meal-dialog">
-        <div class = "dialog-header">
-            <h3>SALMONE AL LIMONE</h3>
-            <button id="close-dialog">&times;</button>
+const info = [
+  {
+    title: "SALMONE AL LIMONE",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet fugit unde inventore asperiores? Odit repudiandae alias quas nisi.",
+    sizes: "Chico, Mediano, Grande",
+    img: "/img/salmone.png",
+  },
+  {
+    title: "INSALATA DI TONNO",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet fugit unde inventore asperiores? Odit repudiandae alias quas nisi.",
+    sizes: "Chico, Mediano, Grande",
+    img: "/img/salmone.png",
+  },
+  {
+    title: "FETTUCCINE ALFREDO",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet fugit unde inventore asperiores? Odit repudiandae alias quas nisi.",
+    sizes: "Chico, Mediano, Grande",
+    img: "/img/salmone.png",
+  },
+];
+
+function mealInfo(number) {
+  // Crear el elemento <dialog>
+  const dialog = document.createElement("dialog");
+
+  // Establecer el contenido del diálogo
+  dialog.innerHTML = `
+        <div class="dialog-header">
+            <h3>${info[number].title}</h3>
+            <button class="close-dialog">&times;</button>
         </div>
 
-        <div>
+        <div style="text-align: center; padding-top:2rem">
+            <p><strong>Información general</strong></p>
+            <p>${info[number].info}</p>
+        </div>
+
+        <div style="text-align: center; padding-top:2rem">
             <p><strong>Tamaño</strong></p>
-            <p>Mediano</p>
+            <p>${info[number].sizes}</p>
         </div>
 
-        <div>
-            <img src="/img/salmone.png" alt="Photo"/>
+        <div style="width:100%; display:flex; justify-content:center;">
+            <img style="width:70%; padding:1rem;" src="${info[number].img}" alt="Photo"/>
         </div>
-    </dialog>
-    `;
-    const dialog = document.getElementById('meal-dialog');
-    dialog.showModal();
+  `;
 
-    document.getElementById('close-dialog').addEventListener('click', () => {
-        dialog.close();
-    });
-});
+  // Agregar el diálogo al cuerpo del documento
+  document.body.appendChild(dialog);
+
+  // Mostrar el diálogo modal
+  dialog.showModal();
+
+  // Agregar el evento para cerrar el diálogo
+  dialog.querySelector(".close-dialog").addEventListener("click", () => {
+    dialog.close();
+    dialog.remove(); // Eliminar el diálogo del DOM después de cerrarlo
+  });
+}
